d583ecb54bb0940549c82aaaa62772e310a3c94d
caalami123 <caalamiagency@gmail.com>
2025-11-20 20:34:32 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545"
+      url: "http://127.0.0.1:8545",
+      accounts: { mnemonic: process.env.MNEMONIC || "test test test test test test test test test test test junk" }
     }
   }
 };
