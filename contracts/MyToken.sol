89e007c3a4fb88016f90ef2418ec7a2c697eda74
caalami123 <caalamiagency@gmail.com>
2025-11-20 21:21:30 +0300

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract TestToken {
-    string public name = "TestToken";
-    string public symbol = "TT";
+contract MyToken {
+    string public name = "MyToken";
+    string public symbol = "MTK";
     uint8 public decimals = 18;
     mapping(address => uint) public balanceOf;
 
     event Transfer(address indexed from, address indexed to, uint amount);
 
-    constructor() {
-        // Initial mint to deployer (optionally leave 0)
+    constructor(uint initialSupply) {
+        // Mint initial supply to deployer
+        balanceOf[msg.sender] = initialSupply;
+        emit Transfer(address(0), msg.sender, initialSupply);
     }
 
     function mint(address to, uint amount) public {
